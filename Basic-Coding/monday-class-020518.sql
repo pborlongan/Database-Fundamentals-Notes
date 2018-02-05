e588a318527b8729fc4c76218ae395ba0c99fe02
patricia <35233489+pborlongan@users.noreply.github.com>
2018-02-05 11:45:59 -0700

    added phone number constraint

--- Basic-Coding/monday-class-020518.sql
+++ Basic-Coding/monday-class-020518.sql
@@ -19,6 +19,8 @@ IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Customers
 -- added IDENTITY 
 -- IDENTITY means the database will generate a unique whole-number value for each column
 
+-- added a DEFAULT constraint
+
 CREATE TABLE Customers
 (
     CustomerNumber  int	
@@ -28,14 +30,26 @@ CREATE TABLE Customers
     LastName        varchar(60)		NOT NULL,
     [Address]       varchar(40)		NOT NULL,
     City            varchar(35)		NOT NULL,
-    Province        char(2)			NOT NULL,
-    PostalCode      char(6)			NOT NULL,	
-    PhoneNumber     char(13)		NULL	  
+    Province        char(2) 
+                    CONSTRAINT DF_Customers_Province
+					DEFAULT ('AB')
+                    NOT NULL,
+    PostalCode      char(6)		
+                    CONSTRAINT CK_Customers_PostalCode
+					Check (PostalCode LIKE '[A-Z][0-9][A-Z][0-9][A-Z][0-9]') -- checks if the data inserted follows the pattern
+                	NOT NULL,	
+    PhoneNumber     char(13)
+					CONSTRAINT CK_Customers_PhoneNumber
+					Check (PhoneNumber LIKE '([0-9][0-9][0-9])[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]')
+                    NULL	  
 )
 
 -- added FOREIGN KEY (WHY DO THESE THINGS HAVE SUPER LONG NAMES)
 -- Foreign Key constraints ensure that when a row of data is being inserted or updated, 
 -- there is a row in the referenced table that has the same value as its Primary Key
+
+-- added CHECK constraint
+-- CHECK constraints ensure that a value is correct
 
 CREATE TABLE Orders
 (
@@ -46,9 +60,15 @@ CREATE TABLE Orders
                 CONSTRAINT FK_Orders_CustomerNumber_Customers_CustomerNumber FOREIGN KEY REFERENCES Customers(CustomerNumber)
         		NOT NULL,
     [Date]          datetime	NOT NULL,
-    Subtotal        money		NOT NULL,
-    GST             money		NOT NULL,
-    Total           money		NULL
+    Subtotal        money
+                CONSTRAINT CK_Orders_Subtotal
+				CHECK (Subtotal > 0)
+                NOT NULL,
+    GST             money
+                CONSTRAINT CK_Orders_GST
+                CHECK (GST >= 0)
+                NOT NULL,
+    Total           AS Subtotal + GST -- this is now a computed column
 )
 
 GO -- END of a batch of instructions
@@ -66,3 +86,13 @@ PRINT 'INSERTING AN ORDER'
 
 INSERT INTO Orders(CustomerNumber, [Date], Subtotal, GST)
     VALUES (100, GETDATE(), 17.45, 0.87)
+
+
+/*
+        constraints:
+            - null/not null
+            - Primary Key
+            - Foreign Key
+            - Check Key
+            - Default Key
+*/
